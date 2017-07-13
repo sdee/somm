b799072a6320ee6fc681e5b5bb01ad1fc3cc78ec
Sutee Dee <thaiyoshi@gmail.com>
2017-07-12 18:43:20 -0700

    customizes options for radar chart. doesn't change scale with values

--- src/components/RadarChart.js
+++ src/components/RadarChart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import ReactBootstrapSlider from 'react-bootstrap-slider';
-
 const { Radar } = require('react-chartjs');
 
 const chartData = {
@@ -21,8 +20,16 @@ const chartData = {
 };
 
 const chartOptions = {
+	scaleOverride: true,
+	scaleSteps: 10,
+	scaleStepWidth: 1,
+	scaleStartValue: 0,
+	maintainAspectRatio: false,
+	scaleShowLabels: true,
+	scaleBeginAtZero: false,
+	pointDot: true,
 	scale: {
-		reverse: true,
+		reverse: false,
 		ticks: {
 			beginAtZero: false,
 			min: 1,
@@ -46,7 +53,6 @@ export default class RadarChart extends React.Component {
 		}
 		chartData.labels = labels;
 		chartData.datasets[0].data = data;
-
 
 		return (
 			<div>
